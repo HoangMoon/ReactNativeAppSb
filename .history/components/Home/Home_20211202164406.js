8f31066e99973b2372392cde42a3019af82953cd
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2021-12-04 13:52:41 +0700

    hoang5

---
+++ .history/components/Home/Home_20211202164406.js
@@ -0,0 +1,366 @@
+import React, {useState} from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+// import Svg, {Use, Image} from 'react-native-svg';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import Swiper from 'react-native-swiper';
+import {DataList, dataSale} from '../../Data/Data';
+import {ProductItem, FeatureItem} from './ProductItem';
+
+const dataBanner = [
+  {
+    idB: 1,
+    imgB: require('../../assets/images/c1.jpg'),
+  },
+  {
+    idB: 2,
+    imgB: require('../../assets/images/c2.jpg'),
+  },
+  {
+    idB: 3,
+    imgB: require('../../assets/images/c3.jpg'),
+  },
+  {
+    idB: 4,
+    imgB: require('../../assets/images/c4.jpg'),
+  },
+  {
+    idB: 5,
+    imgB: require('../../assets/images/c5.jpg'),
+  },
+];
+const Home = ({navigation}) => {
+  const [FoodActive, setFoodActive] = useState(dataSale[0]);
+  const handeActive = item => {
+    setFoodActive(item);
+  };
+  return (
+    <>
+      <ScrollView>
+        <View style={styles.homePage}>
+          <View style={styles.homeTop}>
+            <View style={styles.homeHeader}>
+              <Image
+                style={styles.homeBell}
+                source={require('../../assets/images/bell.png')}
+              />
+              <Image
+                style={styles.homeLogo}
+                source={require('../../assets/images/logo1.png')}
+              />
+              <Image
+                style={styles.homeBar}
+                source={require('../../assets/images/bar.png')}
+              />
+            </View>
+            <View style={styles.homeAvatar}>
+              <View style={styles.avtarTxt}>
+                <Text style={{color: '#fff', fontSize: 25}}>Good morning,</Text>
+                <Text style={styles.avatarName}>Huy Hoàng</Text>
+              </View>
+              <Image source={require('../../assets/images/avatar.png')} />
+            </View>
+            <Swiper
+              style={styles.homeBanner}
+              style={{height: 270, borderRadius: 20, overflow: 'hidden'}}
+              loop
+              autoplay={true}
+              autoplayTimeout={2.5}
+              // dot={<View style={{width: 8, height: 8}}></View>}>
+            >
+              {dataBanner &&
+                dataBanner.length > 0 &&
+                dataBanner.map((item, index) => (
+                  <>
+                    <Image
+                      key={item.idB}
+                      style={styles.bannerImg}
+                      source={item.imgB}
+                    />
+                  </>
+                ))}
+            </Swiper>
+            <View style={styles.homeSearch}>
+              <TextInput
+                style={styles.inputHome}
+                placeholderTextColor={'#fff'}
+                placeholder="Search.."
+              />
+              <Icon
+                style={styles.iconSearch}
+                name="search"
+                size={25}
+                color="#ccc"
+              />
+            </View>
+          </View>
+          <View style={styles.homeContent}>
+            <View style={styles.bannerContent}>
+              <Text style={styles.bannerTitle}>For you</Text>
+              <View style={styles.bannerImgContent}>
+                <View style={styles.bInfo}>
+                  <Image
+                    style={styles.forImg}
+                    source={require('../../assets/images/bn1.png')}
+                  />
+                  <Text style={styles.txtInfo}>
+                    Art in a Cup: Buy One Get One
+                  </Text>
+                </View>
+                <View style={styles.bInfo}>
+                  <Image
+                    style={styles.forImg}
+                    source={require('../../assets/images/bn2.png')}
+                  />
+                  <Text style={styles.txtInfo}>
+                    12th Dec 2021: Pistachio Christmas Tree Frappuccino
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.homeNews}>
+              <Text style={styles.bannerTitle}>News</Text>
+              <View style={styles.newsList}>
+                <View style={styles.newColumn}>
+                  <Image
+                    style={{resizeMode: 'contain'}}
+                    source={require('../../assets/images/view1.png')}
+                  />
+                  <Text style={styles.txtNews}>Elevate Your Tea</Text>
+                </View>
+                <View style={styles.newColumn}>
+                  <Image
+                    style={{resizeMode: 'contain'}}
+                    source={require('../../assets/images/view2.png')}
+                  />
+                  <Text style={styles.txtNews}>Elevate Your Tea</Text>
+                </View>
+                <View style={styles.newColumn}>
+                  <Image
+                    style={{resizeMode: 'contain'}}
+                    source={require('../../assets/images/view3.png')}
+                  />
+                  <Text style={styles.txtNews}>Elevate Your Tea</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.homeProduct}>
+              <Text style={styles.bannerTitle}>Product news</Text>
+              <View style={styles.productList}>
+                {DataList &&
+                  DataList.length > 0 &&
+                  DataList.map((item, index) => (
+                    <>
+                      <ProductItem
+                        image={item.image}
+                        title={item.title}
+                        price={item.price}
+                      />
+                    </>
+                  ))}
+              </View>
+            </View>
+            <View style={styles.homeProduct}>
+              <Text style={styles.bannerTitle}>Feature Product</Text>
+              <ScrollView horizontal={true}>
+                <View style={styles.productTab}>
+                  {dataSale.map(val => (
+                    <TouchableOpacity
+                      onPress={() => handeActive(val)}
+                      style={[
+                        styles.btnTab,
+                        {
+                          backgroundColor:
+                            val.id === FoodActive.id ? '#DDDDDD' : '#FFFFFF',
+                        },
+                      ]}
+                      key={val.id}>
+                      <Text
+                        style={styles.txtBtnTab}
+                        style={{
+                          fontSize: 18,
+                          color:
+                            val.id === FoodActive.id ? '#FFFFFFF' : '#000000',
+                        }}>
+                        {val.name_type}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              </ScrollView>
+
+              <View style={styles.productList}>
+                {FoodActive.data &&
+                  FoodActive.data.length > 0 &&
+                  FoodActive.data.map((item, index) => (
+                    <>
+                      <FeatureItem
+                        key={item.id}
+                        navigation={navigation}
+                        image={item.image}
+                        title={item.title}
+                        price={item.price}
+                        name={item.name}
+                        desc={item.desc}
+                        salePrice={
+                          item.salePrice
+                            ? `Sale ${item.salePrice}`
+                            : `Discount ${Math.floor(Math.random(20) * 30)}`
+                          // : `Discount ${Math.floor(Math.random(20) * 30)}`
+                        }
+                        rating={item.rating}
+                      />
+                    </>
+                  ))}
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  homePage: {
+    flex: 1,
+    height: '100%',
+    backgroundColor: '#00623B',
+    position: 'relative',
+  },
+  homeTop: {
+    padding: 10,
+  },
+  homeHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  homeAvatar: {
+    marginTop: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  avatarName: {
+    color: '#fff',
+    marginTop: 3,
+    fontSize: 25,
+  },
+  bannerImg: {
+    marginTop: 20,
+    width: '100%',
+    height: 240,
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  homeContent: {
+    backgroundColor: '#fff',
+    width: '100%',
+    marginTop: 20,
+    borderRadius: 20,
+    // padding: 20,
+    height: '100%',
+    // position: 'absolute',
+    padding: 10,
+  },
+  bannerTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 18,
+    color: '#314755',
+  },
+  bannerImgContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  forImg: {
+    height: 100,
+    width: '100%',
+  },
+  bInfo: {
+    width: 180,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    marginRight: 15,
+    // marginLeft: 10,
+  },
+  txtInfo: {
+    padding: 10,
+    fontSize: 16,
+    textAlign: 'center',
+    lineHeight: 25,
+  },
+  homeNews: {
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  newsList: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  newColumn: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txtNews: {
+    marginTop: 5,
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  productList: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+  homeSearch: {
+    position: 'relative',
+  },
+  inputHome: {
+    marginTop: 10,
+    borderWidth: 3,
+    borderColor: '#eee',
+    borderRadius: 100,
+    color: '#fff',
+    paddingLeft: 15,
+  },
+  iconSearch: {
+    position: 'absolute',
+    right: 15,
+    top: '40%',
+    // transform: [{translateY: '50%'}],
+  },
+  productTab: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  btnTab: {
+    paddingHorizontal: 12,
+    paddingVertical: 14,
+    borderRadius: 12,
+    marginBottom: 20,
+    shadowColor: '#000',
+    // margin: 20,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default Home;
---
+++ .history/components/Home/ProductItem_20211203094437.js
@@ -0,0 +1,214 @@
+import React from 'react';
+import {StyleSheet, View, Image, Text, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+// import Icon from 'react-native-vector-icons/FontAwesome5';
+
+const ProductItem = props => {
+  return (
+    <>
+      <View style={styles.productItem}>
+        <Image style={styles.productImg} source={props.image} />
+        <Text style={styles.productName}>{props.title}</Text>
+        <Text style={styles.productPrice}>{props.price}$</Text>
+        <Text style={styles.productPriceNews}>News</Text>
+      </View>
+    </>
+  );
+};
+
+const FeatureItem = ({
+  image,
+  title,
+  price,
+  rating,
+  salePrice,
+  navigation,
+  desc,
+  name,
+}) => {
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Details', {
+            image,
+            title,
+            price,
+            rating,
+            salePrice,
+            desc,
+            name,
+            desc,
+          })
+        }>
+        <>
+          <View style={styles.productItem1}>
+            <Image style={styles.productImg} source={image} />
+            <Text style={styles.productName}>{title}</Text>
+            <View style={styles.featurePrice}>
+              <Text style={styles.productPrice}>{price}$</Text>
+            </View>
+            <View style={styles.productRating}>
+              <Text style={styles.txtRating}>{rating}</Text>
+              <Image
+                style={styles.imgRating}
+                source={require('../../assets/images/rating.png')}
+              />
+            </View>
+            <View style={styles.productCart}>
+              <View style={styles.iconCart}>
+                <Icon
+                  style={styles.imgCart}
+                  name="cart-plus"
+                  size={25}
+                  color="#fff"
+                />
+              </View>
+            </View>
+            <Text style={styles.productPriceSale}>{salePrice}</Text>
+            <Icon
+              style={styles.iconHeart}
+              name="heart"
+              size={25}
+              color="#ccc"
+            />
+          </View>
+        </>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  productList: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+    // marginLeft: -5,
+  },
+  productItem: {
+    position: 'relative',
+    Width: 390,
+    minHeight: 350,
+    // borderWidth: 1,
+    // borderColor: '#ccc',
+    marginLeft: 5,
+    marginRight: 15,
+    marginBottom: 30,
+    padding: 10,
+    shadowColor: '#999',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+  productItem1: {
+    position: 'relative',
+    Width: 250,
+    minHeight: 350,
+    // borderWidth: 1,
+    // borderColor: '#ccc',
+    marginBottom: 30,
+    padding: 10,
+    shadowColor: '#999',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+  productImg: {
+    // width: '100%',
+    width: 160,
+    height: 200,
+    marginBottom: 10,
+    resizeMode: 'contain',
+  },
+  productName: {
+    fontSize: 20,
+    marginBottom: 10,
+    color: '#00623B',
+  },
+  productPrice: {
+    fontSize: 18,
+  },
+  featurePrice: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  productPriceSale: {
+    backgroundColor: '#DD5E89',
+    color: '#fff',
+    width: 100,
+    height: 40,
+    lineHeight: 40,
+    fontSize: 18,
+    textAlign: 'center',
+    borderRadius: 10,
+    position: 'absolute',
+    top: -3,
+    left: -5,
+  },
+  productRating: {
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  txtRating: {
+    fontSize: 16,
+    paddingRight: 3,
+  },
+  productCart: {
+    flexDirection: 'row',
+  },
+  iconCart: {
+    backgroundColor: '#00623B',
+    color: '#fff',
+    width: 60,
+    height: 50,
+    position: 'absolute',
+    right: -10,
+    bottom: -40,
+    borderBottomRightRadius: 10,
+    borderTopLeftRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imgCart: {
+    fontSize: 30,
+  },
+  productPriceNews: {
+    backgroundColor: '#EB3349',
+    color: '#fff',
+    width: 100,
+    height: 45,
+    lineHeight: 40,
+    fontSize: 18,
+    textAlign: 'center',
+    borderTopRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  iconHeart: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    color: '#ccc',
+  },
+});
+
+export {ProductItem, FeatureItem};
