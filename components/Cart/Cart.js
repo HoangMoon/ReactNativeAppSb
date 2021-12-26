567504f1e44560dd74c3354d515bbb6ee8900a9a
HoangMoon <hoangmoon8321@gmail.com>
2021-12-26 23:57:35 +0700

    MOON6

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -1,17 +1,20 @@
 import React, {useState} from 'react';
 import {
-  View,
-  Text,
-  Image,
+  FlatList,
+  SafeAreaView,
   StyleSheet,
-  Dimensions,
+  Text,
   TouchableOpacity,
+  View,
+  Image,
+  Dimensions,
   TextInput,
   ScrollView,
 } from 'react-native';
+
 const {width} = Dimensions.get('window');
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import FontIcon from 'react-native-vector-icons/FontAwesome';
+import Countdown from '../Countdown/Countdown';
 const Cart = () => {
   const [count, setCount] = useState(0);
   const handleCountMinus = e => {
@@ -24,236 +27,122 @@ const Cart = () => {
   };
 
   return (
-    <View>
-      <ScrollView>
-        <View style={Styles.Headers}>
-          <View style={Styles.backBtn}>
-            <Icon name="angle-left" style={Styles.Icon}></Icon>
-          </View>
-          <View style={Styles.userBtn}>
+    <ScrollView>
+      <View style={{backgroundColor: '#fff', flex: 1}}>
+        <SafeAreaView>
+          <View style={styles.wrapper}>
+            <TouchableOpacity style={styles.backButton}>
+              <Icon name="angle-left" size={30} style={{color: '#036637'}} />
+            </TouchableOpacity>
             <View>
-              <FontIcon name="bookmark-o" style={Styles.IconBM}></FontIcon>
+              <Text style={styles.headerText}>Cart Items</Text>
             </View>
-            <View>
-              <Text style={Styles.HeadersTex}>Xem Bookmark</Text>
-            </View>
+            <View style={{width: 20}} />
           </View>
-        </View>
-        <View
-          style={{
-            margin: 20,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <Text style={Styles.Title}>Giỏ hàng</Text>
-          <Text>" 3 sp "</Text>
-        </View>
+        </SafeAreaView>
 
-        <View style={Styles.item}>
-          <View style={Styles.Item}>
-            <View>
-              <Image
-                style={Styles.ItemImage}
-                source={require('./assets/images/ShopPic.jpg')}
-              />
-            </View>
-            <View>
-              <Text style={Styles.ItemTitle}>Caramel Frapuchino</Text>
-              <View style={Styles.Price}>
-                <View>
-                  <Text style={Styles.ItemTitle}> 50.000 đ </Text>
-                </View>
-                <TouchableOpacity style={Styles.del}>
-                  <FontIcon name="trash-o" style={Styles.IcMark}></FontIcon>
-                </TouchableOpacity>
-              </View>
-              <View style={{flexDirection: 'row'}}>
-                <View style={Styles.add}>
-                  <TouchableOpacity
-                    style={Styles.actionAdd}
-                    onPress={e => handleCountMinus(e)}>
-                    <Icon style={Styles.iconAdd} name="minus" />
-                  </TouchableOpacity>
-                  <Text style={Styles.txtCount}>{count}</Text>
-                  <TouchableOpacity
-                    style={Styles.actionAdd}
-                    onPress={e => handleCountPlus(e)}>
-                    <Icon style={Styles.iconAdd} name="plus" />
-                  </TouchableOpacity>
-                </View>
-              </View>
+        <View style={styles.Item}>
+          <View>
+            <Image
+              style={styles.ItemImage}
+              source={require('../../assets/images/p6.png')}
+            />
+          </View>
+
+          <View style={{width: width / 2, justifyContent: 'flex-start'}}>
+            <Text style={styles.ItemTitle}>Caramel Frapuchino</Text>
+
+            <Text style={styles.ItemTitle}> 19.99$ </Text>
+
+            <View style={styles.add}>
+              <TouchableOpacity
+                style={styles.actionAdd}
+                onPress={e => handleCountMinus(e)}>
+                <Icon style={styles.iconAdd} name="minus" />
+              </TouchableOpacity>
+              <Text style={styles.txtCount}>{count}</Text>
+              <TouchableOpacity
+                style={styles.actionAdd}
+                onPress={e => handleCountPlus(e)}>
+                <Icon style={styles.iconAdd} name="plus" />
+              </TouchableOpacity>
             </View>
           </View>
         </View>
 
-        <View style={Styles.item}>
-          <View style={Styles.Item}>
-            <View>
-              <Image
-                style={Styles.ItemImage}
-                source={require('./assets/images/ShopPic.jpg')}
-              />
-            </View>
-            <View>
-              <Text style={Styles.ItemTitle}>Caramel Frapuchino</Text>
-              <View style={Styles.Price}>
-                <View>
-                  <Text style={Styles.ItemTitle}> 50.000 đ </Text>
-                </View>
-                <TouchableOpacity style={Styles.del}>
-                  <FontIcon name="trash-o" style={Styles.IcMark}></FontIcon>
-                </TouchableOpacity>
-              </View>
-              <View style={{flexDirection: 'row'}}>
-                <View style={Styles.add}>
-                  <TouchableOpacity
-                    style={Styles.actionAdd}
-                    onPress={e => handleCountMinus(e)}>
-                    <Icon style={Styles.iconAdd} name="minus" />
-                  </TouchableOpacity>
-                  <Text style={Styles.txtCount}>{count}</Text>
-                  <TouchableOpacity
-                    style={Styles.actionAdd}
-                    onPress={e => handleCountPlus(e)}>
-                    <Icon style={Styles.iconAdd} name="plus" />
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
+        <View style={styles.bottomAC}>
+          <View>
+            <Text style={styles.TotalText}>Total Price</Text>
+            <Text style={styles.PriceTex}>19.99$</Text>
           </View>
-        </View>
-
-        <View>
-          <View style={Styles.Item}>
-            <View>
-              <Image
-                style={Styles.ItemImage}
-                source={require('./assets/images/ShopPic.jpg')}
-              />
-            </View>
-
-            <View>
-              <Text style={Styles.ItemTitle}>Caramel Frapuchino</Text>
-              <View style={Styles.Price}>
-                <View>
-                  <Text style={Styles.ItemTitle}> 50.000 đ </Text>
-                </View>
-                <TouchableOpacity style={Styles.del}>
-                  <FontIcon name="trash-o" style={Styles.IcMark}></FontIcon>
-                </TouchableOpacity>
-              </View>
-              <View style={{flexDirection: 'row'}}>
-                <View style={Styles.add}>
-                  <TouchableOpacity
-                    style={Styles.actionAdd}
-                    onPress={e => handleCountMinus(e)}>
-                    <Icon style={Styles.iconAdd} name="minus" />
-                  </TouchableOpacity>
-                  <Text style={Styles.txtCount}>{count}</Text>
-                  <TouchableOpacity
-                    style={Styles.actionAdd}
-                    onPress={e => handleCountPlus(e)}>
-                    <Icon style={Styles.iconAdd} name="plus" />
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-          </View>
-        </View>
-
-        <View style={Styles.bottomAC}>
-          <View style={{flexDirection: 'row'}}>
-            <View>
-              <Text style={Styles.TotalText}>Total Price</Text>
-            </View>
-            <View>
-              <Text style={Styles.PriceTex}>19.99$</Text>
-            </View>
-          </View>
-          <TouchableOpacity
-            style={Styles.Odb}
-            onPress={() => navigation.navigate('Cartu')}>
-            <Text style={Styles.Odbt}>Order Now</Text>
+          <TouchableOpacity style={styles.Odb}>
+            <Text style={styles.Odbt}>Order Now</Text>
           </TouchableOpacity>
         </View>
-      </ScrollView>
-    </View>
+      </View>
+    </ScrollView>
   );
 };
-const Styles = StyleSheet.create({
-  Headers: {
-    width: '100%',
 
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingHorizontal: 20,
+    marginTop: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  backButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F8F8F7',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 0,
+    },
+    shadowRadius: 2,
+    shadowOpacity: 1,
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#616161',
+    textAlign: 'center',
+  },
+  backgroundItem: {
+    backgroundColor: '#f7f6f2',
+    borderRadius: 10,
+    borderWidth: 0.5,
+  },
+  ItemWarpper: {
     marginTop: 20,
-  },
-  backBtn: {
-    width: 30,
-    height: 30,
-    justifyContent: 'center',
-    marginLeft: 20,
-    flexDirection: 'row',
-  },
-  userBtn: {
-    justifyContent: 'center',
-    marginRight: 20,
-    flexDirection: 'row',
-  },
-  Icon: {
-    fontSize: 30,
-    alignItems: 'center',
-    color: '#ccc',
-    marginRight: 20,
-  },
-  IcMark: {
-    fontSize: 25,
-    color: '#1d724d',
-  },
-  IconBM: {
-    fontSize: 18,
-    alignItems: 'center',
-    color: '#ccc',
-    marginRight: 10,
-    color: '#F53959',
-  },
-
-  Price: {flexDirection: 'row', justifyContent: 'space-between'},
-
-  HeadersTex: {
-    fontSize: 15,
-    fontWeight: '800',
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#CCC',
-  },
-  Title: {
-    fontSize: 30,
-    color: 'black',
-    fontWeight: '900',
-  },
-  item: {
-    marginBottom: 20,
+    backgroundColor: '#',
+    borderRadius: 20,
+    marginHorizontal: 20,
   },
   Item: {
+    marginTop: 20,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around',
-    backgroundColor: '#EDEDED',
-    marginHorizontal: 10,
-    borderRadius: 40,
+    backgroundColor: '#FFD8CA',
+    marginHorizontal: 20,
+    borderRadius: 10,
+    // width: width / 2,
   },
   ItemImage: {
-    width: 120,
-    height: 90,
-    // resizeMode: 'contain',
-    borderRadius: 20,
+    width: 90,
+    height: 150,
+    resizeMode: 'contain',
   },
   ItemTitle: {
-    fontWeight: '800',
+    fontWeight: 'bold',
     color: '#555',
-    fontSize: 18,
+    fontSize: 23,
     marginBottom: 5,
   },
   add: {
@@ -261,76 +150,87 @@ const Styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginRight: 100,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#d2d2d1',
     backgroundColor: '#fff',
-    margin: 10,
   },
   addCount: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: '#fff',
     maxWidth: 180,
     height: 40,
     overflow: 'hidden',
+    borderRadius: 10,
+    borderWidth: 1,
   },
   actionAdd: {
-    backgroundColor: '#FF793F',
-    height: 30,
-    width: 30,
+    backgroundColor: '#fff',
+    height: 40,
+    width: 40,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 100,
+    borderRadius: 10,
+    borderWidth: 0.8,
+    borderColor: '#d2d2d1',
+    alignItems: 'center',
   },
   iconAdd: {
     color: '#ccc',
-    fontSize: 15,
+    fontSize: 20,
   },
   txtCount: {
-    fontSize: 18,
+    fontSize: 30,
     paddingLeft: 15,
     paddingRight: 15,
+    color: '#9796f0',
   },
-  bottomAC: {marginTop: 45},
+  bottomAC: {
+    flex: 1,
+    alignItems: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderTopColor: '#d2d2d1',
+    paddingTop: 20,
+    borderTopWidth: 1,
+    borderStyle: 'dashed',
+  },
   TotalText: {
     color: '#227551',
     fontWeight: 'bold',
     fontSize: 18,
     marginBottom: 5,
-    marginLeft: 10,
   },
   PriceTex: {
     color: '#2e2e2e',
     fontWeight: 'bold',
     fontSize: 24,
-    marginLeft: 200,
   },
   Odb: {
     backgroundColor: '#1d724d',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 20,
     borderColor: '#d2d2d1',
     borderWidth: 1,
-    borderRadius: 50,
-    height: 60,
-    paddingHorizontal: 15,
-    margin: 20,
-    lineHeight: 60,
-    textAlign: 'center',
+    borderRadius: 10,
+    height: 40,
   },
   Odbt: {
     color: '#fff',
     textAlign: 'center',
     fontSize: 24,
   },
-  del: {
-    backgroundColor: '#FFCDAF',
-    width: 40,
-    height: 40,
-    borderRadius: 100,
-    flexDirection: 'row',
-    alignItems: 'center',
+  rightAC: {
+    backgroundColor: '#509473',
     justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    flex: 1,
+    width: 60,
   },
 });
 export default Cart;
