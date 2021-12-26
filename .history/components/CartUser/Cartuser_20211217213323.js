29439632977bcb6514c25b7d3980684425c474a1
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2021-12-26 23:09:10 +0700

    hoang12

---
+++ .history/components/CartUser/Cartuser_20211217213323.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import {View, Text, ScrollView, StyleSheet, Image} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Icon1 from 'react-native-vector-icons/Ionicons';
+const Cartuser = () => {
+  const initialRegion = {
+    latitude: 19.97819849719515,
+    longitude: 105.65475851031482,
+    latitudeDelta: 0.09,
+    longitudeDelta: 0.04,
+  };
+  return (
+    <>
+      <ScrollView>
+        <View>
+          <MapView
+            style={styles.cartMaps}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={initialRegion}>
+            <Marker
+              coordinate={{
+                latitude: 19.97819849719515,
+                longitude: 105.65475851031482,
+              }}></Marker>
+          </MapView>
+          <View style={styles.cartUser}>
+            <View>
+              <View style={styles.cartUserContent}>
+                <Image
+                  style={styles.cartUserImg}
+                  source={require('../../assets/images/avatarb.jpg')}
+                />
+                <View>
+                  <Text style={styles.cartUserName}>Huy Hoàng</Text>
+                  <Text style={styles.cartUserVote}>
+                    Tài xế được 100 lượt yêu thích
+                  </Text>
+                  <View style={styles.iconVote}>
+                    <Icon name="star" size={20} style={{color: 'orange'}} />
+                    <Icon name="star" size={20} style={{color: 'orange'}} />
+                    <Icon name="star" size={20} style={{color: 'orange'}} />
+                    <Icon name="star" size={20} style={{color: 'orange'}} />
+                    <Icon name="star" size={20} style={{color: 'orange'}} />
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View style={styles.cartUserGo}>
+              <View>
+                <Text style={styles.cartGotxt}>
+                  Tài xế đang đến - khoảng 5ph
+                </Text>
+              </View>
+              <View style={styles.iconCall}>
+                <Icon1 style={{color: '#fff', fontSize: 18}} name="call" />
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  cartMaps: {
+    // height: '100%',
+    flex: 1,
+    width: 400,
+    height: 450,
+  },
+  cartUser: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  cartUserContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  cartUserImg: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    borderRadius: 130,
+    marginRight: 10,
+  },
+  cartUserName: {
+    fontSize: 18,
+    color: '#000',
+    fontWeight: '600',
+    marginBottom: 5,
+  },
+  cartUserVote: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  iconVote: {
+    flexDirection: 'row',
+  },
+  cartUserGo: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  cartGotxt: {
+    fontSize: 17,
+    color: 'green',
+  },
+  iconCall: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'green',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Cartuser;
---
+++ .history/components/Introduce/Introduce_20211222134615.js
@@ -0,0 +1,111 @@
+import Swiper from 'react-native-swiper';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+
+const introData = [
+  {
+    idT: 1,
+    introHeading: 'Good Coffee , Good Moods',
+    introDesc: `“To inspire and nurture the human spirit–one person, one cup and one
+    neighborhood at a time.”`,
+    introImg: require('../../assets/images/intro1.png'),
+  },
+  {
+    idT: 2,
+    introHeading: ' Starbucks Frappuccino Work can wait',
+    introDesc: `“Bring a friend and enjoy a Frappuccino. 
+    Find stores in your area.”`,
+    introImg: require('../../assets/images/bn4.png'),
+  },
+  {
+    idT: 3,
+    introHeading: 'Morning begins with Tropical Splash Startbucks',
+    introDesc: `“Bring the Fantasy Tail Frappuccino, or treat yourself to the boldly refreshing Peach Cloud with Jelly. “`,
+    introImg: require('../../assets/images/intro3.png'),
+  },
+];
+
+const Introduce = ({navigation}) => {
+  return (
+    <Swiper style={styles.wrapper}>
+      {introData.map((item, index) => (
+        <>
+          <View style={styles.slide3} key={item.idT}>
+            <Image
+              style={styles.introLogo}
+              style={{resizeMode: 'contain'}}
+              source={require('../../assets/images/logo.png')}
+            />
+            <Image style={styles.introApp1} source={item.introImg} />
+            <View style={styles.introContent}>
+              <Text style={styles.introHeading}>{item.introHeading}</Text>
+              <Text style={styles.introDesc}>{item.introDesc}</Text>
+            </View>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <View style={styles.introApp2}>
+                <Text style={{fontSize: 20, fontWeight: 'bold', color: '#fff'}}>
+                  Get Started
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </>
+      ))}
+    </Swiper>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide3: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'White',
+    justifyContent: 'center',
+    justifyContent: 'space-between',
+    textAlign: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  introLogo: {
+    marginTop: 30,
+  },
+  introHeading: {
+    fontSize: 25,
+    color: '#00623B',
+    maxWidth: 300,
+    textAlign: 'center',
+  },
+  introDesc: {
+    marginTop: 10,
+    maxWidth: 300,
+    textAlign: 'center',
+    fontSize: 16,
+    lineHeight: 25,
+  },
+  introApp2: {
+    height: 50,
+    width: 300,
+    backgroundColor: '#22A45D',
+    color: '#fff',
+    marginBottom: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  introContent: {
+    textAlign: 'center',
+  },
+});
+export default Introduce;
